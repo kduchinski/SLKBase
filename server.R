c54d4fdaf8ec7eddf5a4d4a6ec797760f543e50e
kduchinski <kduchinski@gmail.com>
2017-10-21 21:39:08 -0400

    Enrichment toggle + color code

--- server.R
+++ server.R
@@ -6,22 +6,24 @@ library(pathview)
 
 shinyServer(function(input, output){
   
-  SUMLines_DB <-  dbConnect(RMySQL::MySQL(),
-                            username = "root",
-                            password = "sumlines",
-                            host = "35.190.141.134",
-                            port = 3306,
-                            dbname = "SUMLines_DB"
-  )
-  tables = dbListTables(SUMLines_DB)
-  dbDisconnect(SUMLines_DB)
-  sumLineList = c()
-  i = 1
-  while(i < length(tables)){
-    sumLineList = append(sumLineList, strsplit(tables[i], '_')[[1]][1])
-    i = i + 3
-  }
-
+  # SUMLines_DB <-  dbConnect(RMySQL::MySQL(),
+  #                           username = "root",
+  #                           password = "sumlines",
+  #                           host = "35.190.141.134",
+  #                           port = 3306,
+  #                           dbname = "SUMLines_DB"
+  # )
+  # tables = dbListTables(SUMLines_DB)
+  # tables = tables[2:length(tables)]
+  # dbDisconnect(SUMLines_DB)
+  # sumLineList = c()
+  # i = 1
+  # while(i < length(tables)){
+  #   sumLineList = append(sumLineList, strsplit(tables[i], '_')[[1]][1])
+  #   i = i + 3
+  # }
+  sumLineList = c("SUM149", "SUM185", "SUM190", "SUM225", "SUM229", "SUM44", "SUM52")
+  
   output$sumlineSelect <- renderUI({
     selectInput("sumline", "Cell line:", choices = sumLineList, selected = sumLineList[1])
   })
@@ -36,32 +38,55 @@ shinyServer(function(input, output){
                               dbname = "SUMLines_DB"
     )
     on.exit(dbDisconnect(SUMLines_DB))
-    rs.data = dbSendQuery(SUMLines_DB, paste0('select ids,quantlog from ', input$sumline, '_CellectaData'))
-    df.data = fetch(rs.data, n=-1)
+    
+    rs.fc = dbSendQuery(SUMLines_DB, paste0('select Symbol,', input$sumline, ' from ExpressionDataSUMLinesAllGenes'))
+    df.fc = fetch(rs.fc, n=-1)
     dbClearResult(dbListResults(SUMLines_DB)[[1]])
-    rs.mut = dbSendQuery(SUMLines_DB, paste0('select gene from ', input$sumline, '_Mut_COSMIC'))
-    df.mut = fetch(rs.mut, n=-1)
-    df.data$COSMIC = df.data[,2]
     
-    cutoff = quantile(df.data[,2])[4] + 1.5*IQR(df.data[,2])
     
-    df.data$COSMIC[df.data[,1] %in% df.mut[,1]] = -1*cutoff
+    if(input$enrich == "Gene Expression"){
+      # rs.fc = dbSendQuery(SUMLines_DB, paste0('select Symbol,', input$sumline, ' from ExpressionDataSUMLinesAllGenes'))
+      # df.fc = fetch(rs.fc, n=-1)
+      # dbClearResult(dbListResults(SUMLines_DB)[[1]])
+      df.data = df.fc[(df.fc[,2] > 1 | df.fc[,2] < -1),]
+      cutoff = ceiling(max(df.data[,2]))
+    }else{
+      rs.mut = dbSendQuery(SUMLines_DB, paste0('select gene from ', input$sumline, '_Mut_COSMIC'))
+      df.mut = fetch(rs.mut, n=-1)
+      dbClearResult(dbListResults(SUMLines_DB)[[1]])
+      rs.amp = dbSendQuery(SUMLines_DB, paste0('select Symbol from ', input$sumline, '_amplificationData'))
+      df.amp = fetch(rs.amp, n=-1)
+      dbClearResult(dbListResults(SUMLines_DB)[[1]])
+      
+      df.overexp = df.fc[df.fc[,2] > 1, 1]
+      rs.data = dbSendQuery(SUMLines_DB, paste0('select ids,quantlog from ', input$sumline, '_CellectaData where quantloghit = 1'))
+      df.data = fetch(rs.data, n=-1)
+      dbClearResult(dbListResults(SUMLines_DB)[[1]])
+      if(input$sumline == "SUM185"){ cutoff = max(df.data[,2])}
+      else{ cutoff = quantile(df.data[,2])[4] + 1.5*IQR(df.data[,2])}
+    }
     
-    head(df.data)
-    df.mat = as.matrix(df.data[,2:3])
+    df.data$Buffer1 = df.data[,2]
+    df.data$Buffer2 = df.data[,2]
+    df.data$Buffer3 = df.data[,2]
+    df.data$Mutated = df.data[,2]
+    df.data$Amplified = df.data[,2]
+    df.data$Overexpressed = df.data[,2]
     
-    df.mat[df.mat[,1] > cutoff] = cutoff
-    df.mat[df.mat[,2] > cutoff] = cutoff
-    df.mat[,1] = df.mat[,1]/cutoff
-    df.mat[,2] = df.mat[,2]/cutoff
+    if(input$enrich == "shRNA Screen"){
+    df.data$Mutated[df.data[,1] %in% df.mut[,1]] = -1*cutoff
+    df.data$Amplified[df.data[,1] %in% df.amp[,1]] = 0
+    df.data$Overexpressed[df.data[,1] %in% df.overexp] = NA
+    }
+    
+    df.mat = as.matrix(df.data[,-1])
     
     rownames(df.mat) = id2eg(df.data[,1])[,2]
-    colnames(df.mat) = c("QuantLog", "COSMIC")
-
+    
     keggID = strsplit(input$pathway, '_')[[1]][2]
     outfile <- paste0("./hsa", keggID, ".pathview.multi.png")
-    pathview(gene.data = df.mat[,1:2], pathway.id = keggID, species = "hsa", 
-             limit=list(gene = 1, cpd = 1),  kegg.dir = tempdir())
+    pathview(gene.data = df.mat, pathway.id = keggID, species = "hsa", 
+             limit=list(gene = cutoff, cpd = 1),  kegg.dir = tempdir())
     list(src = outfile,
          contentType = 'image/png',
          width = 1000,
@@ -81,15 +106,17 @@ shinyServer(function(input, output){
     rs.cell = dbSendQuery(SUMLines_DB, paste0('select ids,quantlog,quantlogrank from ', input$sumline, '_CellectaData where ids = "', input$gene, '"'))
     df.cell = fetch(rs.cell, n=-1)
     dbClearResult(dbListResults(SUMLines_DB)[[1]])
-    rs.amp = dbSendQuery(SUMLines_DB, paste0('select `log Fold Change`,`DNA Amp.` from ', input$sumline, '_amplificationData where Symbol = "', input$gene, '"'))
+    rs.amp = dbSendQuery(SUMLines_DB, paste0('select `DNA Amp.` from ', input$sumline, '_amplificationData where Symbol = "', input$gene, '"'))
     df.amp = fetch(rs.amp, n=-1)
+    dbClearResult(dbListResults(SUMLines_DB)[[1]])
+    rs.fc = dbSendQuery(SUMLines_DB, paste0('select ', input$sumline, ' from ExpressionDataSUMLinesAllGenes where Symbol = "', input$gene, '"'))
+    df.fc = fetch(rs.fc, n=-1)
     dbClearResult(dbListResults(SUMLines_DB)[[1]])
     rs.mut = dbSendQuery(SUMLines_DB, paste0('select geneMutation,Occurences_in_COSMIC from ', input$sumline, '_Mut_COSMIC where gene = "', input$gene, '"'))
     df.mut = fetch(rs.mut, n=-1)
     if(nrow(df.cell) == 0){return(NULL)}
     df = df.cell
     if(nrow(df.amp) == 0){
-      df$log_Fold_Change = c("NA")
       df$DNA_Amp = c("NA")
     }else{
       df = cbind(df.cell, df.amp)
@@ -99,6 +126,11 @@ shinyServer(function(input, output){
       df$Occurences_in_COSMIC = c("NA")
     }else{
       df = cbind(df,df.mut)
+    }
+    if(nrow(df.fc) == 0){
+      df$log_Fold_Change = c("NA")
+    }else{
+      df$log_Fold_Change = df.fc[,1]
     }
     df
   }, options = c(paging = F, searching = F))
--- ui.R
+++ ui.R
@@ -1,44 +1,8 @@
 library(shiny)
 
 shinyUI(fluidPage(
-  # navbarPage("SLKBase",
-             # tabPanel("Welcome",
-             #          titlePanel("The SUM Breast Cancer Cell Line Knowledge Base"),
-             #          sidebarLayout(
-             #            sidebarPanel(
-             #              img(src = 'cropped-sextant.png'),
-             #              h3("Sextant Oncology"),
-             #              h4("The SUM breast cancer cell line
-             #                 knowledge base"),
-             #              h6("Welcome to the first edition of the SUM breast cancer cell line Knowledge Base. 
-             #                 Several years ago, I and my laboratory developed a series of breast cancer cell 
-             #                 lines from biopsy specimens obtained from patients being treated at the University 
-             #                 of Michigan Comprehensive Cancer Center.")
-             #            ),
-             #            mainPanel(
-             #              br(),
-             #              br(),
-             #              tags$blockquote(h3("This is your one-stop resource for knowledge, data,
-             #                 and information relevant to the SUM series of human
-             #                 breast cancer cell lines, developed in the Ethier
-             #                 laboratory while at the University of Michigan.")),
-             #              br(),
-             #              h4("Here is what you'll find in these pages:"),
-             #              h5("A brief description of how each cell line was derived,
-             #                 including information on the patient from which the cells
-             #                 were isolated"),
-             #              h5("A bibliography that contains every paper ever published that
-             #                 contains data obtained with that particular cell line"),
-             #              h5("Links to a number of KEGG pathways where all of the genomic
-             #                 data obtained with each cell line is displayed in a pathway-specific
-             #                 manner. This is also your gateway to viewing all of the data on
-             #                 copy number, mutation status, expression level, and hit score for
-             #                 each annotated gene in the pathway. There will also be a narrated
-             #                 summary of the results for each pathway.")
-             #            )
-             #          )),
-             # tabPanel("Pathway Visualization",
-                      fluidRow(
+                  
+                  fluidRow(
                             h3("Pathway and Gene Search"),
                             h6("Welcome! Please select a SUM cell line and a KEGG pathway to see a pathway diagram
                                enriched by a Cellecta RNAi screen."),
@@ -46,23 +10,14 @@ shinyUI(fluidPage(
                             h6("To search for a particular pathway, press the backspace
                                key and type either the pathway name or KEGG pathway ID."),
                             selectizeInput("pathway", label = "Pathway:", choices = read.csv("./www/kegg.txt", header = FALSE)[1]),
+                            radioButtons("enrich", label = "Enrichment Dataset:", choices = c("shRNA Screen", "Gene Expression")),
                             h6("For information on a gene in the selected cell line, use the search bar below."),
                             textInput("gene", label = "Gene:"),
                             fluidRow(column(4, dataTableOutput("geneInfo")))
                         ),
-                        fluidRow(
-                          imageOutput("pathway")
+                    fluidRow(
+                            imageOutput("pathway")
                         )
-                       )
-             # navbarMenu("Cell lines",
-             #            tabPanel("SUM149"),
-             #            tabPanel("SUM185"),
-             #            tabPanel("SUM190"),
-             #            tabPanel("SUM225"),
-             #            tabPanel("SUM229"),
-             #            tabPanel("SUM44"),
-             #            tabPanel("SUM52")
-             #            )
-             # )
+                    )
   
 )
